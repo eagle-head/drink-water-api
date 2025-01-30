c035b93d0d812e45857024ef50095febe6112ddd
eagle-head <edy.kohn@gmail.com>
2025-01-30 20:34:56 -0300

    Add givenExistingPublicId_whenCreateUser_thenThrowEmailAlreadyUsedException.

--- src/test/java/br/com/drinkwater/usermanagement/service/UserServiceTest.java
+++ src/test/java/br/com/drinkwater/usermanagement/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.drinkwater.usermanagement.service;
 
+import br.com.drinkwater.usermanagement.exception.EmailAlreadyUsedException;
 import br.com.drinkwater.usermanagement.mapper.UserMapper;
 import br.com.drinkwater.usermanagement.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,8 @@ import static br.com.drinkwater.usermanagement.constants.ResponseUserDTOConstant
 import static br.com.drinkwater.usermanagement.constants.UserConstants.JOHN_DOE;
 import static br.com.drinkwater.usermanagement.constants.UserDTOConstants.JOHN_DOE_DTO;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -43,5 +44,65 @@ public class UserServiceTest {
         verify(this.userMapper).toEntity(JOHN_DOE_DTO);
         verify(this.userRepository).save(JOHN_DOE);
         verify(this.userMapper).toDto(JOHN_DOE);
+    }
+
+    @Test
+    public void givenExistingPublicId_whenCreateUser_thenThrowEmailAlreadyUsedException() {
+        when(this.userRepository.existsByPublicId(JOHN_DOE.getPublicId())).thenReturn(true);
+
+        assertThatThrownBy(() -> this.userService.createUser(JOHN_DOE.getPublicId(), JOHN_DOE_DTO))
+                .isInstanceOf(EmailAlreadyUsedException.class);
+
+        verify(this.userRepository).existsByPublicId(JOHN_DOE.getPublicId());
+    }
+
+    @Test
+    public void givenValidPublicId_whenGetUserByPublicId_thenReturnResponseUserDTO() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenInvalidPublicId_whenGetUserByPublicId_thenThrowUserNotFoundException() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenValidUserAndUpdateData_whenUpdateUser_thenReturnUpdatedUserDTO() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenInvalidPublicId_whenUpdateUser_thenThrowUserNotFoundException() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenValidPublicId_whenDeleteUser_thenUserShouldBeDeleted() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenInvalidPublicId_whenDeleteUser_thenThrowUserNotFoundException() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenValidPublicId_whenFindByPublicId_thenReturnUser() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenInvalidPublicId_whenFindByPublicId_thenThrowUserNotFoundException() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenNonExistingPublicId_whenValidateUserExistence_thenShouldNotThrowException() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void givenExistingPublicId_whenValidateUserExistence_thenThrowEmailAlreadyUsedException() {
+        throw new RuntimeException("Test not implemented");
     }
 }
